297419ba4f86d8c794700b8d7d1238ecd0070029
Sohan Tirole <152252832+SohanTirole@users.noreply.github.com>
2026-02-03 20:26:33 +0530

    Changes Uploaded

--- Test8.java
+++ Test8.java
@@ -8,10 +8,16 @@ class Test {
         }
           
         for(int i=1 ; i<arr.length ; i++){
-            
+            int temp = arr[i];
+            int j = i;
+            while(j>0 && arr[j-1]>temp){
+                arr[j] = arr[j-1];
+                j = j-1;
+            }
+            arr[j] = temp;
         }
 
-        System.out.println("After Sorting");
+        System.out.println("\nAfter Sorting");
         for(int a : arr){
             System.out.print(a+", ");
         }
